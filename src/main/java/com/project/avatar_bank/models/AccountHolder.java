c129819f6072aa494bcce7e6d05c2127237ee300
Blezzy dela Cruz <blezzydelacruz@Blezzys-MacBook-Air.local>
2024-03-12 13:12:36 +0000

    created account holder model

---
+++ src/main/java/com/project/avatar_bank/models/AccountHolder.java
@@ -0,0 +1,99 @@
+package com.project.avatar_bank.models;
+
+import java.time.LocalDate;
+
+public class AccountHolder {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private EmploymentStatus employmentStatus;
+
+    private String address;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDate dateOfBirth;
+
+    public AccountHolder(String firstName, String lastName, EmploymentStatus employmentStatus, String address, String email, String phoneNumber, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employmentStatus = employmentStatus;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public AccountHolder() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public EmploymentStatus getEmploymentStatus() {
+        return employmentStatus;
+    }
+
+    public void setEmploymentStatus(EmploymentStatus employmentStatus) {
+        this.employmentStatus = employmentStatus;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
